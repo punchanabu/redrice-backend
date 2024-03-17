c7437f46f087d435e69cbd37f6786910df8116f5
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-03-17 19:27:25 +0700

    refactor: routes configuration

--- routers/router.go
+++ routers/router.go
@@ -13,37 +13,38 @@ import (
 func UseRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
-	
+
 	docs.SwaggerInfo.Title = "RedRice API"
 	docs.SwaggerInfo.Description = "This is a server for managing restaurant with RedRice API build with Go Gin and Gorm"
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-
 
 	r.POST("/login", api.Login)
 	r.POST("/register", api.Register)
 	apiv1 := r.Group("/api/v1")
 	apiv1.Use(middleware.Auth())
 	{
-		// users
-		apiv1.GET("/users", v1.GetUsers)
-		apiv1.POST("/users", v1.CreateUser)
-		apiv1.GET("/users/:id", v1.GetUser)
-		apiv1.PUT("/users/:id", v1.UpdateUser)
-		apiv1.DELETE("/users/:id", v1.DeleteUser)
-
-		// restaurants
+		// for authorized user
 		apiv1.GET("/restaurants", v1.GetRestaurants)
-		apiv1.POST("/restaurants", v1.CreateRestaurant)
 		apiv1.GET("/restaurants/:id", v1.GetRestaurant)
-		apiv1.PUT("/restaurants/:id", v1.UpdateRestaurant)
-		apiv1.DELETE("/restaurants/:id", v1.DeleteRestaurant)
-
-		// reservation
 		apiv1.GET("/reservations", v1.GetReservations)
-		apiv1.POST("/reservations", v1.CreateReservation)
 		apiv1.GET("/reservations/:id", v1.GetReservation)
-		apiv1.PUT("/reservations/:id", v1.UpdateReservation)
+		apiv1.GET("/users", v1.GetUsers)
+		apiv1.GET("/users/:id", v1.GetUser)
+		apiv1.POST("/reservations", v1.CreateReservation)
+
+		adminRoutes := apiv1.Group("/")
+		adminRoutes.Use(middleware.Admin())
+		{
+			adminRoutes.POST("/users", v1.CreateUser)
+			adminRoutes.PUT("/users/:id", v1.UpdateUser)
+			adminRoutes.DELETE("/users/:id", v1.DeleteUser)
+			adminRoutes.POST("/restaurants", v1.CreateRestaurant)
+			adminRoutes.PUT("/reservations/:id", v1.UpdateReservation)
+			adminRoutes.PUT("/restaurants/:id", v1.UpdateRestaurant)
+			adminRoutes.DELETE("/restaurants/:id", v1.DeleteRestaurant)
+		}
+
 	}
 	return r
 }
