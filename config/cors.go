143d45fde04a79ef090879a4750ea8286d75d3ae
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-03-25 03:33:40 +0700

    feat: add allow all origin

--- config/cors.go
+++ config/cors.go
@@ -5,7 +5,7 @@ import (
 )
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Or your specific allowed origin
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*") 
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
